c8e30b1f6e463818c572b8f9111df9e224bea538
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-09-10 13:46:42 +0300

    dependencies clean up

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var protractor = require("gulp-protractor").protractor;
 
 gulp.task('sass', function () {
     gulp.src('./sass/**/*.scss')
@@ -12,20 +11,4 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-gulp.task('e2etests:run', function() {
-    gulp.src(["./src/tests/*.js"])
-        .pipe(protractor({
-            configFile: "spec/protractor.conf.js",
-            args: ['--baseUrl', 'http://127.0.0.1:8000']
-        }))
-        .on('error', function(e) { throw e })
-});
-
-var jscs = require('gulp-jscs');
-
-gulp.task('jscs', function () {
-    return gulp.src('js/*.js')
-        .pipe(jscs());
 });
